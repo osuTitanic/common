eaf9e8f946f4d0d70cce7eb26cc380cdd4ae3bb3
Lekuru <contact@lekuru.xyz>
2023-08-08 17:55:03 +0200

    Implement rank histories

--- database/repositories/histories.py
+++ database/repositories/histories.py
@@ -1,11 +1,13 @@
 
-from datetime import datetime
+from app.common.cache import leaderboards
 from app.common.database.objects import (
     DBReplayHistory,
     DBPlayHistory,
     DBRankHistory,
     DBStats
 )
+
+from datetime import datetime
 
 import app
 
@@ -69,5 +71,23 @@ def update_rank(
     stats: DBStats,
     country: str
 ) -> None:
-    # TODO
-    pass
+    country_rank = leaderboards.country_rank(stats.user_id, stats.mode, country)
+    global_rank = leaderboards.global_rank(stats.user_id, stats.mode)
+    score_rank = leaderboards.score_rank(stats.user_id, stats.mode)
+
+    if global_rank <= 0:
+        return
+
+    with app.session.database.session as session:
+        session.add(
+            DBRankHistory(
+                stats.user_id,
+                stats.mode,
+                stats.rscore,
+                stats.pp,
+                global_rank,
+                country_rank,
+                score_rank
+            )
+        )
+        session.commit()
