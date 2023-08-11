388cfbaeec22739c70aacc3528df164f7e02cfec
Lekuru <contact@lekuru.xyz>
2023-08-11 18:46:32 +0200

    Use selectin eager loading in beatmap fetch queries

--- database/repositories/beatmaps.py
+++ database/repositories/beatmaps.py
@@ -2,22 +2,27 @@
 from app.common.database.objects import DBBeatmap
 from typing import Optional, List
 
+from sqlalchemy.orm import selectinload
+
 import app
 
 # TODO: create
 
 def fetch_by_id(id: int) -> Optional[DBBeatmap]:
     return app.session.database.session.query(DBBeatmap) \
+                .options(selectinload(DBBeatmap.beatmapset)) \
                 .filter(DBBeatmap.id == id) \
                 .first()
 
 def fetch_by_file(filename: str) -> Optional[DBBeatmap]:
     return app.session.database.session.query(DBBeatmap) \
+                .options(selectinload(DBBeatmap.beatmapset)) \
                 .filter(DBBeatmap.filename == filename) \
                 .first()
 
 def fetch_by_checksum(checksum: str) -> Optional[DBBeatmap]:
     return app.session.database.session.query(DBBeatmap) \
+                .options(selectinload(DBBeatmap.beatmapset)) \
                 .filter(DBBeatmap.md5 == checksum) \
                 .first()
 
