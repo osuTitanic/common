92951fec312398c7bbde3e099378b05f950b00a2
Lekuru <contact@lekuru.xyz>
2023-08-16 19:02:10 +0200

    Fix osu version regex

--- constants/regexes.py
+++ constants/regexes.py
@@ -3,8 +3,7 @@ import re
 
 OSU_VERSION = re.compile(
     r"^b(?P<date>\d{1,8})"
-    r"(?:(?P<name>[\w]*))?"
-    r"(\.?)"
-    r"(?:(?P<revision>\d))?"
-    r"(?P<stream>dev|tourney|test|peppy)?$",
+    r"(?:(?P<name>(?!dev|tourney|test|peppy\b)\w+\b))?"
+    r"(?:\.(?P<revision>\d{1,2}|))?"
+    r"(?P<stream>dev|tourney|test|peppy)?$"
 )
