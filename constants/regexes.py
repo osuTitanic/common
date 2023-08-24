6fc41a7c4a3ba11fe2df44d166e4f0e629909ebe
Lekuru <contact@lekuru.xyz>
2023-08-24 18:36:01 +0200

    Add 'ubertest' subversion

--- constants/regexes.py
+++ constants/regexes.py
@@ -3,7 +3,7 @@ import re
 
 OSU_VERSION = re.compile(
     r"^b(?P<date>\d{1,8})"
-    r"(?:(?P<name>(?!dev|tourney|test|peppy|arcade\b)\w+\b))?"
+    r"(?:(?P<name>(?!dev|tourney|test|peppy|arcade|ubertest\b)\w+\b))?"
     r"(?:\.(?P<revision>\d{1,2}|))?"
-    r"(?P<stream>dev|tourney|test|peppy|arcade)?$"
+    r"(?P<stream>dev|tourney|test|peppy|arcade|ubertest)?$"
 )
