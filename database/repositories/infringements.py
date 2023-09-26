4a89271c50c904be88df0aaaec17d605341023cd
Levi <contact@lekuru.xyz>
2023-09-26 11:04:41 +0200

    Implement infringement queries

--- database/repositories/infringements.py
+++ database/repositories/infringements.py
@@ -12,16 +12,43 @@ def create(
     description: Optional[str] = None,
     is_permanent: bool = False
 ) -> DBInfringement:
-    ...
+    with app.session.database.managed_session() as session:
+        session.add(
+            i := DBInfringement(
+                user_id,
+                action,
+                length,
+                description,
+                is_permanent
+            )
+        )
+        session.commit()
+        session.refresh(i)
+
+    return i
 
 def fetch_recent(user_id: int) -> Optional[DBInfringement]:
-    ...
+    return app.session.database.session.query(DBInfringement) \
+                    .filter(DBInfringement.user_id == user_id) \
+                    .order_by(DBInfringement.id.desc()) \
+                    .first()
 
 def fetch_recent_by_action(user_id: int, action: int) -> List[DBInfringement]:
-    ...
+    return app.session.database.session.query(DBInfringement) \
+                    .filter(DBInfringement.user_id == user_id) \
+                    .filter(DBInfringement.action == action) \
+                    .order_by(DBInfringement.id.desc()) \
+                    .first()
 
 def fetch_all(user_id: int) -> List[DBInfringement]:
-    ...
+    return app.session.database.session.query(DBInfringement) \
+                    .filter(DBInfringement.user_id == user_id) \
+                    .order_by(DBInfringement.id.desc()) \
+                    .all()
 
 def fetch_all_by_action(user_id: int, action: int) -> List[DBInfringement]:
-    ...
+    return app.session.database.session.query(DBInfringement) \
+                    .filter(DBInfringement.user_id == user_id) \
+                    .filter(DBInfringement.action == action) \
+                    .order_by(DBInfringement.time.desc()) \
+                    .all()
