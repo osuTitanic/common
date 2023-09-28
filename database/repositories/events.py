ecfc1fb6917e70744756e0ec2002429270895073
Lekuru <contact@lekuru.xyz>
2023-09-28 23:03:27 +0200

    Add queries for events & matches

---
+++ database/repositories/events.py
@@ -0,0 +1,49 @@
+
+from app.common.database.objects import DBMatchEvent
+from app.common.constants import EventType
+from typing import List, Optional
+
+import app
+
+def create(
+    match_id: int,
+    type: EventType,
+    data: dict = {},
+) -> DBMatchEvent:
+    with app.session.database.managed_session() as session:
+        session.add(
+            m := DBMatchEvent(
+                match_id,
+                type.value,
+                data
+            )
+        )
+        session.commit()
+        session.refresh(m)
+
+    return m
+
+def fetch_last(match_id: int) -> Optional[DBMatchEvent]:
+    return app.session.database.session.query(DBMatchEvent) \
+        .filter(DBMatchEvent.match_id == match_id) \
+        .order_by(DBMatchEvent.time.desc()) \
+        .first()
+
+def fetch_last_by_type(match_id: int, type: int) -> Optional[DBMatchEvent]:
+    return app.session.database.session.query(DBMatchEvent) \
+        .filter(DBMatchEvent.match_id == match_id) \
+        .filter(DBMatchEvent.type == type) \
+        .order_by(DBMatchEvent.time.desc()) \
+        .first()
+
+def fetch_all(match_id: int) -> List[DBMatchEvent]:
+    return app.session.database.session.query(DBMatchEvent) \
+        .filter(DBMatchEvent.match_id == match_id) \
+        .all()
+
+def delete_all(match_id: int) -> None:
+    with app.session.database.managed_session() as session:
+        session.query(DBMatchEvent) \
+            .filter(DBMatchEvent.match_id == match_id) \
+            .delete()
+        session.commit()
---
+++ database/repositories/matches.py
@@ -0,0 +1,52 @@
+
+from app.common.database.repositories import events
+from app.common.database.objects import DBMatch
+
+from typing import Optional
+
+import app
+
+def create(
+    name: str,
+    bancho_id: int,
+    creator_id: int,
+) -> DBMatch:
+    with app.session.database.managed_session() as session:
+        session.add(
+            m := DBMatch(
+                name,
+                creator_id,
+                bancho_id
+            )
+        )
+        session.commit()
+        session.refresh(m)
+
+    return m
+
+def fetch_by_id(id: int) -> Optional[DBMatch]:
+    return app.session.database.session.query(DBMatch) \
+        .filter(DBMatch.id == id) \
+        .first()
+
+def fetch_by_bancho_id(id: int) -> Optional[DBMatch]:
+    return app.session.database.session.query(DBMatch) \
+        .filter(DBMatch.bancho_id == id) \
+        .first()
+
+def update(id: int, updates: dict) -> None:
+    with app.session.database.managed_session() as session:
+        session.query(DBMatch) \
+            .filter(DBMatch.id == id) \
+            .update(updates)
+        session.commit()
+
+def delete(id: int) -> None:
+    # Delete events first
+    events.delete_all(id)
+
+    with app.session.database.managed_session() as session:
+        session.query(DBMatch) \
+            .filter(DBMatch.id == id) \
+            .delete()
+        session.commit()
