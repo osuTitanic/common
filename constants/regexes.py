3a545a0f03e8ae1e83fdd7a83a4c681a8e91a9f7
Lekuru <contact@lekuru.xyz>
2023-11-16 23:26:32 +0100

    Add email & username regex

--- constants/regexes.py
+++ constants/regexes.py
@@ -7,3 +7,11 @@ OSU_VERSION = re.compile(
     r"(?:\.(?P<revision>\d{1,2}|))?"
     r"(?P<stream>dev|tourney|test|peppy|arcade|ubertest)?$"
 )
+
+EMAIL = re.compile(
+    r"^[^@\s]{1,200}@[^@\s\.]{1,30}(?:\.[^@\.\s]{2,24})+$"
+)
+
+USERNAME = re.compile(
+    r'^[a-zA-Z0-9|^\-{}_|\[\]]+$'
+)
