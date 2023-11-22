0c560d50d8580cdda558ef3e37251baeca4d348e
Lekuru <contact@lekuru.xyz>
2023-11-22 14:02:19 +0100

    Remove ttl cache from beatmaps

--- database/repositories/beatmaps.py
+++ database/repositories/beatmaps.py
@@ -3,8 +3,6 @@ from app.common.database.objects import DBBeatmap
 from sqlalchemy.orm import selectinload
 from sqlalchemy import func
 
-from ...helpers.caching import ttl_cache
-
 from typing import Optional, List
 from datetime import datetime
 
@@ -56,28 +54,24 @@ def create(
 
     return m
 
-@ttl_cache(ttl=60*60)
 def fetch_by_id(id: int) -> Optional[DBBeatmap]:
     return app.session.database.session.query(DBBeatmap) \
                 .options(selectinload(DBBeatmap.beatmapset)) \
                 .filter(DBBeatmap.id == id) \
                 .first()
 
-@ttl_cache(ttl=60*60)
 def fetch_by_file(filename: str) -> Optional[DBBeatmap]:
     return app.session.database.session.query(DBBeatmap) \
                 .options(selectinload(DBBeatmap.beatmapset)) \
                 .filter(DBBeatmap.filename == filename) \
                 .first()
 
-@ttl_cache(ttl=60*60)
 def fetch_by_checksum(checksum: str) -> Optional[DBBeatmap]:
     return app.session.database.session.query(DBBeatmap) \
                 .options(selectinload(DBBeatmap.beatmapset)) \
                 .filter(DBBeatmap.md5 == checksum) \
                 .first()
 
-@ttl_cache(ttl=60*60)
 def fetch_by_set(set_id: int) -> List[DBBeatmap]:
     return app.session.database.session.query(DBBeatmap) \
                 .filter(DBBeatmap.set_id == set_id) \
