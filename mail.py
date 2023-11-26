dd95d2890978c4f0532074c36bf95ae1cb0d19e9
Lekuru <contact@lekuru.xyz>
2023-11-26 23:23:08 +0100

    Add new emails

--- mail.py
+++ mail.py
@@ -47,3 +47,44 @@ def send_password_reset_email(verification: DBVerification, user: DBUser):
         message,
         user.email
     )
+
+def send_password_changed_email(user: DBUser):
+    message = email.PASSWORD_CHANGED.format(
+        domain=config.DOMAIN_NAME,
+        username=user.name,
+        signature=email.SIGNATURE.format(domain=config.DOMAIN_NAME)
+    )
+
+    return send(
+        'Your password was changed',
+        message,
+        user.email
+    )
+
+def send_email_changed(user: DBUser):
+    message = email.EMAIL_CHANGED.format(
+        domain=config.DOMAIN_NAME,
+        username=user.name,
+        signature=email.SIGNATURE.format(domain=config.DOMAIN_NAME)
+    )
+
+    return send(
+        'Your email address was changed',
+        message,
+        user.email
+    )
+
+def send_reactivate_account_email(verification: DBVerification, user: DBUser):
+    message = email.REACTIVATE_ACCOUNT.format(
+        domain=config.DOMAIN_NAME,
+        username=user.name,
+        verification_id=verification.id,
+        verification_token=verification.token,
+        signature=email.SIGNATURE.format(domain=config.DOMAIN_NAME)
+    )
+
+    return send(
+        'Reactivate your account',
+        message,
+        user.email
+    )
