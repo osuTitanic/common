b8edf8902486cac544d329bcfdde19877a2ac277
Levi <contact@lekuru.xyz>
2023-11-29 09:17:56 +0100

    Code formatting

--- helpers/score.py
+++ helpers/score.py
@@ -1,8 +1,11 @@
+
 from ..database.objects import DBBeatmap, DBScore
 from ..constants.mods import Mods
 
 def calculate_difficulty_multiplier(beatmap: DBBeatmap):
-    return round((beatmap.hp + beatmap.cs + beatmap.od + min(0, max(16, beatmap.max_combo / beatmap.total_length * 8))) / 38 * 5)
+    return round(
+        (beatmap.hp + beatmap.cs + beatmap.od + min(0, max(16, beatmap.max_combo / beatmap.total_length * 8))) / 38 * 5
+    )
 
 def calculate_mod_multiplier(mods: int):
     multiplier = 1.00
@@ -30,8 +33,11 @@ def calculate_rx_score(score: DBScore) -> int:
     total_hits = score.n300 + score.n100 + score.n50 + score.nMiss
     avg_hit = (300*(score.n300/total_hits)) + (100*(score.n100/total_hits)) + (50*(score.n50/total_hits))
     final_score = score.total_score - (avg_hit * score.max_combo)
+
     mod_multiplier = calculate_mod_multiplier(score.mods)
     difficulty_multiplier = calculate_difficulty_multiplier(score.beatmap)
+
     for combo in range(1, score.max_combo):
         final_score += avg_hit * (1 + (combo * difficulty_multiplier * mod_multiplier / 25))
+
     return round(final_score)
