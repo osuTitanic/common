36a98aace9d19c0eb053df3ddc1634fc3b95ec6b
Lekuru <contact@lekuru.xyz>
2023-12-04 19:36:36 +0100

    Refactor function formatting

--- helpers/score.py
+++ helpers/score.py
@@ -7,7 +7,12 @@ import functools
 def calculate_difficulty_multiplier(beatmap: DBBeatmap, total_hits: int):
     """Get the beatmap difficulty multiplier for score calculations"""
     return round(
-        (beatmap.hp + beatmap.cs + beatmap.od + min(0, max(16, total_hits / beatmap.total_length * 8))) / 38 * 5
+        (
+            beatmap.hp +
+            beatmap.cs +
+            beatmap.od +
+            min(0, max(16, total_hits / beatmap.total_length * 8))
+        ) / 38 * 5
     )
 
 def calculate_mod_multiplier(mods: Mods) -> float:
@@ -22,27 +27,41 @@ def calculate_mod_multiplier(mods: Mods) -> float:
         Mods.Nightcore: 1.12,
         Mods.Flashlight: 1.12,
         Mods.Relax: 0.65,
-        Mods.Autopilot: 0.65
+        Mods.Autopilot: 0.65,
     }
 
     multiplier = functools.reduce(
         (lambda acc, mod: acc * multipliers[mod]),
-        mods,
-        1.00
+        mods, 1.00
     )
 
     return 1.00 * multiplier
 
 def calculate_rx_score(score: DBScore) -> int:
-    """Recalculate the total score for relax plays"""
-    total_hits = score.n300 + score.n100 + score.n50 + score.nMiss
-    avg_hit = (300*(score.n300/total_hits)) + (100*(score.n100/total_hits)) + (50*(score.n50/total_hits))
-    final_score = score.total_score - (avg_hit * score.max_combo)
+    """Calculate the total score for relax plays"""
+    total_hits = (
+        score.n300 +
+        score.n100 +
+        score.n50 +
+        score.nMiss
+    )
+
+    avg_hit = (
+        (300 * (score.n300 / total_hits)) +
+        (100 * (score.n100 / total_hits)) +
+        (50 * (score.n50 / total_hits))
+    )
+
+    final_score = (
+        score.total_score - (avg_hit * score.max_combo)
+    )
 
     mod_multiplier = calculate_mod_multiplier(Mods(score.mods))
     difficulty_multiplier = calculate_difficulty_multiplier(score.beatmap, total_hits)
 
     for combo in range(1, score.max_combo):
-        final_score += avg_hit * (1 + (combo * difficulty_multiplier * mod_multiplier / 25))
+        final_score += avg_hit * (
+            1 + (combo * difficulty_multiplier * mod_multiplier / 25)
+        )
 
     return round(final_score)
