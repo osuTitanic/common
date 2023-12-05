4b819061ed43bbcc80102e951888343b411bf5ad
Lekuru <contact@lekuru.xyz>
2023-12-05 17:43:47 +0100

    Fix rx nerf

--- helpers/score.py
+++ helpers/score.py
@@ -26,8 +26,8 @@ def calculate_mod_multiplier(mods: Mods) -> float:
         Mods.DoubleTime: 1.12,
         Mods.Nightcore: 1.12,
         Mods.Flashlight: 1.12,
-        Mods.Relax: 0.60,
-        Mods.Autopilot: 0.60,
+        Mods.Relax: 0.5,
+        Mods.Autopilot: 0.5,
     }
 
     multiplier = functools.reduce(
