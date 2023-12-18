0ad1947ef4e4522f86deb5dba09f4639e574bf57
Lekuru <contact@lekuru.xyz>
2023-12-18 01:09:23 +0100

    Add `session_wrapper` function decorator

---
+++ database/repositories/wrapper.py
@@ -0,0 +1,23 @@
+
+from sqlalchemy.orm import Session
+from functools import wraps
+
+import app
+
+def session_wrapper(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if kwargs.get('session'):
+            # Use existing session
+            return func(*args, **kwargs)
+
+        if args and isinstance(args[-1], Session):
+            # Use existing session
+            return func(*args, **kwargs)
+
+        with app.session.database.managed_session() as session:
+            # Get new session for this function
+            kwargs.update({'session': session})
+            return func(*args, **kwargs)
+
+    return wrapper
