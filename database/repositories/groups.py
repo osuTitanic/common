9cdbc6ff5d57959be1362009fa1321ab9857ee8a
Lekuru <contact@lekuru.xyz>
2023-12-20 21:44:04 +0100

    Add `include_hidden` parameter

--- database/repositories/groups.py
+++ database/repositories/groups.py
@@ -14,15 +14,26 @@ def fetch_one(id: int, session: Session | None = None) -> DBGroup | None:
         .first()
 
 @session_wrapper
-def fetch_user_groups(user_id: int, session: Session | None = None) -> List[DBGroup]:
-    return session.query(DBGroup) \
-        .join(DBGroupEntry) \
-        .filter(DBGroupEntry.user_id == user_id) \
-        .all()
-
-@session_wrapper
 def fetch_group_users(group_id: int, session: Session | None = None) -> List[DBUser]:
     return session.query(DBUser) \
         .join(DBGroupEntry) \
         .filter(DBGroupEntry.group_id == group_id) \
+        .all()
+
+@session_wrapper
+def fetch_user_groups(
+    user_id: int,
+    include_hidden: bool = False,
+    session: Session | None = None
+) -> List[DBGroup]:
+    if include_hidden:
+        return session.query(DBGroup) \
+            .join(DBGroupEntry) \
+            .filter(DBGroupEntry.user_id == user_id) \
+            .all()
+
+    return session.query(DBGroup) \
+        .join(DBGroupEntry) \
+        .filter(DBGroupEntry.user_id == user_id) \
+        .filter(DBGroup.hidden == False) \
         .all()
