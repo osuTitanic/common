e4933631c5f102e0d3f7b9c5424fb94b48bdca63
Levi <contact@lekuru.xyz>
2024-01-05 15:07:04 +0100

    Add ip address resolvers

--- helpers/ip.py
+++ helpers/ip.py
@@ -28,3 +28,47 @@ def is_local_ip(ip: str) -> bool:
             return True
 
     return False
+
+def resolve_ip_address_flask(request):
+    """Resolve the IP address of a flask request"""
+    ip = request.headers.get("CF-Connecting-IP")
+    forwards = None
+
+    if ip is None:
+        forwards = request.headers.get("X-Forwarded-For")
+
+    if forwards:
+        ip = forwards.split(",")[0]
+    else:
+        ip = request.headers.get("X-Real-IP")
+
+    if ip is None:
+        ip = request.environ['REMOTE_ADDR']
+
+    return ip.strip()
+
+def resolve_ip_address_fastapi(request):
+    """Resolve the IP address of a fastapi request"""
+    if ip := request.headers.get("CF-Connecting-IP"):
+        return ip
+
+    if forwards := request.headers.get("X-Forwarded-For"):
+        return forwards.split(",")[0]
+
+    if ip := request.headers.get("X-Real-IP"):
+        return ip
+
+    return request.client.host.strip()
+
+def resolve_ip_address_twisted(request):
+    """Resolve the IP address of a twisted request"""
+    if ip := request.requestHeaders.getRawHeaders("CF-Connecting-IP"):
+        return ip[0]
+
+    if forwards := request.requestHeaders.getRawHeaders("X-Forwarded-For"):
+        return forwards[0]
+
+    if ip := request.requestHeaders.getRawHeaders("X-Real-IP"):
+        return ip[0]
+
+    return request.getClientAddress().host.strip()
