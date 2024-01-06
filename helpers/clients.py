04cf3a43ce02255bc5f6a91abc13660e11ec5e94
Lekuru <contact@lekuru.xyz>
2024-01-06 18:35:41 +0100

    Refactor manifest fetching

--- helpers/clients.py
+++ helpers/clients.py
@@ -1,5 +1,8 @@
 
+
 from .caching import ttl_cache
+from .. import officer
+
 from typing import List
 
 import config
@@ -7,12 +10,31 @@ import app
 
 @ttl_cache(ttl=3600)
 def get_manifest() -> List[dict]:
-    response = app.session.requests.get(f'http://osu.{config.DOMAIN_NAME}/clients/manifest.json')
+    app.session.logger.debug('Fetching client manifest...')
 
-    if not response.ok:
-        return []
+    try:
+        response = app.session.requests.get(
+            f'http://osu.{config.DOMAIN_NAME}/clients/manifest.json'
+        )
 
-    return response.json()
+        if not response.ok:
+            return []
+
+        clients = response.json()
+
+        app.session.logger.debug(
+            'Client manifest fetched successfully. '
+            f'({len(clients)} clients)'
+        )
+
+        return clients
+    except Exception as e:
+        app.session.logger.error(
+            f'Failed to fetch client manifest: {e}',
+            exc_info=e
+        )
+
+    return []
 
 def get_client_hashes() -> List[str]:
     return [
@@ -22,4 +44,10 @@ def get_client_hashes() -> List[str]:
     ]
 
 def is_valid_client_hash(hash: str) -> bool:
-    return hash in get_client_hashes()
+    if not (hashes := get_client_hashes()):
+        officer.call(
+            f'Failed to get client hashes, assuming valid. ({hash})'
+        )
+        return True
+
+    return hash in hashes
