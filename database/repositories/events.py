05fff7581131180acae054e6b7af20222b354952
Levi <contact@lekuru.xyz>
2024-01-13 17:05:07 +0100

    Add `fetch_all_after_time` query

--- database/repositories/events.py
+++ database/repositories/events.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 from app.common.database.objects import DBMatchEvent
 from app.common.constants import EventType
 from sqlalchemy.orm import Session
+from datetime import datetime
 from typing import List
 
 from .wrapper import session_wrapper
@@ -45,6 +46,18 @@ def fetch_last_by_type(match_id: int, type: int, session: Session | None = None)
 def fetch_all(match_id: int, session: Session | None = None) -> List[DBMatchEvent]:
     return session.query(DBMatchEvent) \
         .filter(DBMatchEvent.match_id == match_id) \
+        .all()
+
+@session_wrapper
+def fetch_all_after_time(
+    match_id: int,
+    after: datetime,
+    session: Session | None = None
+) -> List[DBMatchEvent]:
+    return session.query(DBMatchEvent) \
+        .filter(DBMatchEvent.match_id == match_id) \
+        .filter(DBMatchEvent.time > after) \
+        .order_by(DBMatchEvent.time.asc()) \
         .all()
 
 @session_wrapper
