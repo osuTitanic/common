b95b7ed7a85dda3d7cf7f45ad97a1bdf4db270fd
Levi <contact@lekuru.xyz>
2024-01-19 23:35:14 +0100

    Update ppv1 calculations based on public gist

--- helpers/performance/ppv1.py
+++ helpers/performance/ppv1.py
@@ -1,71 +1,96 @@
 
 from __future__ import annotations
 
+from ...database.objects import DBScore, DBBeatmap
 from ...database.repositories import scores
-from ...database.objects import DBScore
+from ...constants import Mods
 
 from sqlalchemy.orm import Session
+from sqlalchemy import func
 from datetime import datetime
 from typing import List
 
 import math
 import app
 
-def calculate_weight(pps: List[float]) -> float:
-    """Calculate the sum of weighted pp for each score"""
-    pps.sort(reverse=True)
-    return sum(pp * 0.95**index for index, pp in enumerate(pps))
+# ppv1 reference: https://gist.github.com/peppy/4f8fcb6629d300c56ebe80156b20b76c
 
-def calculate_ppv1(
-    score: DBScore,
-    age_falloff: int = 365,
-    rank_falloff: int = 500,
-    populariy_falloff: int = 1000,
-    session: Session | None = None
-) -> float:
+def calculate_star_rating(beatmap: DBBeatmap) -> float:
+    """Calculate the old eyup star rating"""
+    # TODO: Add an actual implementation
+    return min(5, beatmap.diff * 0.565)
+
+def calculate_ppv1(score: DBScore, session: Session | None = None) -> float:
     """Calculate ppv1, by using the score's pp as a difficulty factor"""
     if score.beatmap.playcount <= 0:
         return 0
-
-    score_age = (datetime.now() - score.submitted_at.replace(tzinfo=None)).days
-    populariy_factor = math.log(score.beatmap.playcount, populariy_falloff)
-
-    age_factor = (
-        math.log(-1 * score_age + (age_falloff + 1), age_falloff + 1) + 0.01
-        if score_age <= age_falloff else 0.01
-    )
 
     score_rank = scores.fetch_score_index_by_tscore(
         score.total_score,
         score.beatmap.id,
         score.mode,
-        session
+        session=session
     )
 
-    rank_factor = (
-        min(1, (-1 * math.log(score_rank, rank_falloff) + 1) + 0.01)
-        if score_rank <= rank_falloff else 0.01
-    )
+    mods = Mods(score.mods)
 
-    acc_bonus = 1.2 if score.acc == 1 else 1
-    fc_bonus = 1.15 if score.perfect and score.acc < 1 else 1
+    # TODO: Use old eyup star rating
+    star_rating = calculate_star_rating(score.beatmap)
 
-    return (score.pp * score.acc * populariy_factor * age_factor * rank_factor * acc_bonus * fc_bonus)
+    base_pp = math.pow(star_rating, 4) / math.pow(score_rank, 0.5)
 
-def calculate_weighted_ppv1(
-    scores: List[DBScore],
-    age_falloff: int = 365,
-    rank_falloff: int = 500,
-    populariy_falloff: int = 1000
-) -> float:
+    # Older scores will give less pp
+    score_age = (datetime.now() - score.submitted_at.replace(tzinfo=None)).days
+    age_factor = max(0.01, 1 - 0.01 * (score_age / 10))
+
+    # Bonus for SS's & FC's
+    ss_bonus = 1.36 if score.acc == 1 else 1
+    fc_bonus = 1.2 if score.perfect and score.acc != 1 else 1
+
+    # Adjustments for mods
+    hr_bonus = 1.1 if (Mods.HardRock in mods) else 1
+    dt_bonus = 1.1 if (Mods.DoubleTime in mods) or (Mods.Nightcore in mods) else 1
+    rx_nerf  = 0.6 if (Mods.Relax in mods) or (Mods.Autopilot in mods) else 1
+    ez_nerf  = 0.2 if (Mods.Easy in mods) or (Mods.HalfTime in mods) else 1
+
+    # NOTE: Beatmap popularity is nefed a LOT, since it would inflate pp to the roof
+    populariy_factor = math.pow(score.beatmap.playcount, 0.145)
+    acc_factor = math.pow(score.acc, 15)
+
+    # Nerf converts
+    if score.mode > 0 and score.mode != score.beatmap.mode:
+        base_pp *= 0.2
+
+    # Nerf "easy maps"... idk?
+    if score.mode != 1 and (score.beatmap.passcount / score.beatmap.playcount) > 0.3:
+        base_pp *= 0.2
+
+    result_pp = math.prod([
+        base_pp,
+        age_factor,
+        ss_bonus,
+        fc_bonus,
+        hr_bonus,
+        dt_bonus,
+        rx_nerf,
+        ez_nerf,
+        populariy_factor,
+        acc_factor
+    ])
+
+    return max(0, result_pp)
+
+def calculate_weight(pps: List[float]) -> float:
+    """Calculate the sum of weighted pp for each score"""
+    pps.sort(reverse=True)
+    return sum(pp * 0.95**index for index, pp in enumerate(pps))
+
+def calculate_weighted_ppv1(scores: List[DBScore]) -> float:
     """Calculate weighted ppv1 with from a list of scores"""
     with app.session.database.managed_session() as session:
         return calculate_weight([
             calculate_ppv1(
                 score,
-                age_falloff,
-                rank_falloff,
-                populariy_falloff,
                 session
             )
             for score in scores
