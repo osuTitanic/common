48c5b88ea218ff149a887cc335d9e81892b757c5
Lekuru <contact@lekuru.xyz>
2024-05-03 18:06:31 +0200

    Add ttl cache to resource mirror queries

--- database/repositories/resources.py
+++ database/repositories/resources.py
@@ -4,18 +4,21 @@
 from __future__ import annotations
 
 from app.common.database.objects import DBResourceMirror
+from app.common.helpers.caching import ttl_cache
 from sqlalchemy.orm import Session
 from typing import List
 
 from .wrapper import session_wrapper
 
 @session_wrapper
+@ttl_cache(ttl=60)
 def fetch_all(session: Session = ...) -> List[DBResourceMirror]:
     return session.query(DBResourceMirror) \
         .order_by(DBResourceMirror.priority.asc()) \
         .all()
 
 @session_wrapper
+@ttl_cache(ttl=60)
 def fetch_all_by_server(server: int, session: Session = ...) -> List[DBResourceMirror]:
     return session.query(DBResourceMirror) \
         .filter(DBResourceMirror.server == server) \
@@ -23,6 +26,7 @@ def fetch_all_by_server(server: int, session: Session = ...) -> List[DBResourceM
         .all()
 
 @session_wrapper
+@ttl_cache(ttl=60)
 def fetch_by_type(type: int, server: int, session: Session = ...) -> List[DBResourceMirror]:
     return session.query(DBResourceMirror) \
         .filter(DBResourceMirror.type == type) \
