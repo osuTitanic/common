a976d5bbe82f740bceda750e25f3a7f7eacb62bc
Lekuru <contact@lekuru.xyz>
2024-05-09 15:00:38 +0200

    Add `fetch_hashes_by_filename` function

--- helpers/clients.py
+++ helpers/clients.py
@@ -3,6 +3,15 @@
 from ..database.repositories import releases
 from typing import List
 
+def fetch_hashes_by_filename(filename: str) -> List[str]:
+    return [
+        hash
+        for release in releases.fetch_all()
+        for file in release.hashes
+        if file['file'] == filename
+        for hash in file['md5']
+    ]
+
 def fetch_hashes(version: int) -> List[str]:
     return [
         hash
