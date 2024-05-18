6581f2c1b499b6f678a5db6e738a6f9d6247fa47
Lekuru <contact@lekuru.xyz>
2024-05-19 01:03:03 +0200

    Refactor user stats restore function

--- database/repositories/stats.py
+++ database/repositories/stats.py
@@ -85,7 +85,11 @@ def fetch_all(user_id: int, session: Session = ...) -> List[DBStats]:
 
 @session_wrapper
 def restore(user_id: int, session: Session = ...) -> None:
-    all_stats = [DBStats(user_id, mode) for mode in range(4)]
+    """Recalculate stats from scratch"""
+    all_stats = [
+        DBStats(user_id, mode)
+        for mode in range(4)
+    ]
 
     for mode in range(4):
         score_count = session.query(DBScore) \
@@ -113,14 +117,9 @@ def restore(user_id: int, session: Session = ...) -> None:
             .filter(DBScore.user_id == 15) \
             .filter(DBScore.mode == 0) \
             .filter(DBScore.status > 1) \
-            .first()
+            .first() or [0]
 
-        if map_times:
-            map_times = map_times[-1]
-        else:
-            map_times = 0
-
-        playtime = map_times + fail_times
+        playtime = map_times[-1] + fail_times
 
         combo_score = session.query(DBScore) \
             .filter(DBScore.user_id == user_id) \
@@ -128,20 +127,17 @@ def restore(user_id: int, session: Session = ...) -> None:
             .order_by(DBScore.max_combo.desc()) \
             .first()
 
-        if combo_score:
-            max_combo = combo_score.max_combo
-        else:
-            max_combo = 0
+        max_combo = (
+            combo_score.max_combo
+            if combo_score else 0
+        )
 
         total_score = session.query(
             func.sum(DBScore.total_score)
         ) \
             .filter(DBScore.user_id == user_id) \
             .filter(DBScore.mode == mode) \
-            .scalar()
-
-        if total_score is None:
-            total_score = 0
+            .scalar() or 0
 
         stats: DBStats = all_stats[mode]
         stats.playcount = score_count
@@ -152,72 +148,62 @@ def restore(user_id: int, session: Session = ...) -> None:
         top_scores = scores.fetch_top_scores(
             user_id,
             mode,
-            exclude_approved=(
-                not config.APPROVED_MAP_REWARDS
-            )
+            exclude_approved=(not config.APPROVED_MAP_REWARDS)
         )
 
-        # Update acc and pp
-
-        score_count_best = scores.fetch_count(
-            user_id,
-            mode
-        )
-
-        if score_count_best > 0:
+        if top_scores:
+            # Update accuracy
             total_acc = 0
             divide_total = 0
 
             for index, s in enumerate(top_scores):
                 add = 0.95 ** index
-                total_acc    += s.acc * add
+                total_acc += s.acc * add
                 divide_total += add
 
-            if divide_total != 0:
-                stats.acc = total_acc / divide_total
-            else:
-                stats.acc = 0.0
+            stats.acc = (
+                total_acc / divide_total
+                if divide_total != 0 else 0.0
+            )
 
+            # Update pp
             weighted_pp = sum(score.pp * 0.95**index for index, score in enumerate(top_scores))
-            bonus_pp = 416.6667 * (1 - 0.9994**score_count_best)
-
+            bonus_pp = 416.6667 * (1 - 0.9994**len(top_scores))
             stats.pp = weighted_pp + bonus_pp
 
-    best_scores = session.query(DBScore) \
-        .filter(DBScore.user_id == user_id) \
-        .filter(DBScore.status == 3) \
-        .all()
+            # Update rscore
+            stats.rscore = sum(
+                score.total_score for score in top_scores
+            )
 
-    for score in best_scores:
-        stats: DBStats = all_stats[score.mode]
+        # Update grades
+        grades = scores.fetch_grades(
+            stats.user_id,
+            stats.mode,
+            session=session
+        )
 
-        grade_count = eval(f'stats.{score.grade.lower()}_count')
+        for grade, count in grades.items():
+            setattr(
+                stats,
+                f'{grade.lower()}_count',
+                count
+            )
 
-        if not grade_count:
-            grade_count = 0
+        # Update total hits
+        total_hits_formula = {
+            0: DBScore.n50 + DBScore.n100 + DBScore.n300,
+            1: DBScore.n50 + DBScore.n100 + DBScore.n300,
+            2: DBScore.n50 + DBScore.n100 + DBScore.n300 + DBScore.nKatu,
+            3: DBScore.n50 + DBScore.n100 + DBScore.n300 + DBScore.nKatu + DBScore.nGeki
+        }
 
-        if not stats.rscore:
-            stats.rscore = 0
-
-        if not stats.total_hits:
-            stats.total_hits = 0
-
-        stats.rscore += score.total_score
-        grade_count += 1
-
-        if stats.mode == 2:
-            # ctb
-            total_hits = score.n50 + score.n100 + score.n300 + score.nMiss + score.nKatu
-
-        elif stats.mode == 3:
-            # mania
-            total_hits = score.n300 + score.n100 + score.n50 + score.nGeki + score.nKatu + score.nMiss
-
-        else:
-            # standard + taiko
-            total_hits = score.n50 + score.n100 + score.n300 + score.nMiss
-
-        stats.total_hits += total_hits
+        stats.total_hits = session.query(
+            func.sum(total_hits_formula[stats.mode])
+        ) \
+            .filter(DBScore.user_id == stats.user_id) \
+            .filter(DBScore.mode == stats.mode) \
+            .scalar() or 0
 
     for stats in all_stats:
         session.add(stats)
