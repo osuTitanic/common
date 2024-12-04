2b1847e8a254c4a5e14a35544f3ad0a7fd4610e8
Lekuru <contact@lekuru.xyz>
2024-12-04 19:06:29 +0100

    Add cached sizes to `remove_avatar`

--- storage.py
+++ storage.py
@@ -386,6 +386,9 @@ class Storage:
         self.remove_from_cache(f'avatar:{id}')
         self.remove(id, 'avatars')
 
+        for size in (25, 120, 120, 128, 256):
+            self.remove_from_cache(f'avatar:{id}:{size}')
+
     def save(self, key: str, content: bytes, bucket: str) -> bool:
         """Save a file to the specified bucket/directory."""
         if config.S3_ENABLED:
