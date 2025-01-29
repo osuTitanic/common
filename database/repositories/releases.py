179b7448e84c6fb00c96b686d28e9f0ddf0f0027
Levi <contact@lekuru.xyz>
2025-01-29 18:43:18 +0100

    Add `releases.create` query

--- database/repositories/releases.py
+++ database/repositories/releases.py
@@ -10,6 +10,26 @@ from typing import List
 from .wrapper import session_wrapper
 
 @session_wrapper
+def create(
+    name: str,
+    version: int,
+    downloads: list,
+    hashes: dict,
+    screenshots: dict,
+    session: Session = ...
+) -> DBRelease:
+    session.add(
+        release := DBRelease(
+            name=name,
+            version=version,
+            hashes=hashes
+        )
+    )
+    session.refresh(release)
+    session.commit()
+    return release
+
+@session_wrapper
 def fetch_by_name(name: str, session: Session = ...) -> DBRelease | None:
     return session.query(DBRelease) \
         .filter(DBRelease.name == name) \
