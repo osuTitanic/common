a6682194072b2832f45bc68d2f7714df74a7c6f0
Lekuru <contact@lekuru.xyz>
2025-03-23 15:12:08 +0100

    Refactor release checks

--- helpers/clients.py
+++ helpers/clients.py
@@ -1,26 +1,31 @@
 
 
 from ..database.repositories import releases
+from sqlalchemy.orm import Session
 from typing import List
 
-def fetch_hashes_by_filename(filename: str) -> List[str]:
-    return [
-        hash
-        for release in releases.fetch_all()
-        for file in release.hashes
-        if file['file'] == filename
-        for hash in file['md5']
-    ]
+@releases.session_wrapper
+def is_valid_client_hash(version: int, hash: str, session: Session = ...) -> bool:
+    hashes = fetch_hashes(version, session)
+    return hash in hashes
 
-def fetch_hashes(version: int) -> List[str]:
+@releases.session_wrapper
+def is_valid_mod(identifier: str, hash: str, session: Session = ...) -> bool:
+    if not (release := releases.fetch_modded(identifier)):
+        return False
+
+    for file in release.hashes:
+        if file.endswith('.exe') and hash in release.hashes[file]:
+            return True
+
+    return False
+
+@releases.session_wrapper
+def fetch_hashes(version: int, session: Session = ...) -> List[str]:
     return [
         hash
-        for release in releases.fetch_hashes(version) or []
+        for release in releases.fetch_hashes(version, session) or []
         for entry in release[0]
         for hash in entry['md5']
         if entry['file'].startswith('osu') and entry['file'].endswith('.exe')
     ]
-
-def is_valid_client_hash(version: int, hash: str) -> bool:
-    hashes = fetch_hashes(version)
-    return hash in hashes
