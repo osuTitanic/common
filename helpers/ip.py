0fabb7e637339bf3ad7a6da68da577d8df4655c9
Lekuru <contact@lekuru.xyz>
2025-04-17 20:16:13 +0200

    Use built-in methods for checking `is_local_ip`

--- helpers/ip.py
+++ helpers/ip.py
@@ -2,32 +2,20 @@
 import ipaddress
 
 def is_local_ip(ip: str) -> bool:
-    address = ipaddress.ip_address(ip)
-
-    if address.version == 6:
-        private = [
-            ipaddress.IPv6Network('fc00::/7'),
-            ipaddress.IPv6Network('::1/128')
-        ]
-
-        for net in private:
-            if address in net:
-                return True
-
+    """Check if the given IP address is a local IP address"""
+    try:
+        address = ipaddress.ip_address(ip)
+    except ValueError:
         return False
 
-    private = [
-        ipaddress.IPv4Network('127.0.0.0/8'),
-        ipaddress.IPv4Network('192.168.0.0/16'),
-        ipaddress.IPv4Network('172.16.0.0/12'),
-        ipaddress.IPv4Network('10.0.0.0/8')
-    ]
-
-    for net in private:
-        if address in net:
-            return True
-
-    return False
+    return any((
+        address.is_private,
+        address.is_loopback,
+        address.is_link_local,
+        address.is_reserved,
+        address.is_multicast,
+        address.is_unspecified
+    ))
 
 def resolve_ip_address_flask(request):
     """Resolve the IP address of a flask request"""
