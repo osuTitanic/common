9d46ac25bc9b8a3199f6c52797fca06143a16878
Lekuru <contact@lekuru.xyz>
2025-04-28 18:44:25 +0200

    Add `ratings.fetch_ratio` queries

--- database/repositories/ratings.py
+++ database/repositories/ratings.py
@@ -1,6 +1,6 @@
 
 from __future__ import annotations
-from typing import List, Dict
+from typing import List, Dict, Tuple
 
 from app.common.database.objects import DBRating
 from sqlalchemy.orm import Session
@@ -78,6 +78,34 @@ def fetch_range_by_set(set_id: int, session: Session = ...) -> Dict[int, int]:
         rating: count
         for rating, count in result
     }
+
+@session_wrapper
+def fetch_ratio(beatmap_hash: str, session: Session = ...) -> Tuple[int, int]:
+    result = session.query(
+        func.count(DBRating.rating).filter(DBRating.rating >= 5).label('count_good'),
+        func.count(DBRating.rating).filter(DBRating.rating < 5).label('count_bad')
+    ) \
+        .filter(DBRating.map_checksum == beatmap_hash) \
+        .first()
+
+    if result is None:
+        return 0, 0
+
+    return result[0] or 0, result[1] or 0
+
+@session_wrapper
+def fetch_ratio_by_set(set_id: int, session: Session = ...) -> Tuple[int, int]:
+    result = session.query(
+        func.count(DBRating.rating).filter(DBRating.rating >= 5).label('count_good'),
+        func.count(DBRating.rating).filter(DBRating.rating < 5).label('count_bad')
+    ) \
+        .filter(DBRating.set_id == set_id) \
+        .first()
+
+    if result is None:
+        return 0, 0
+
+    return result[0] or 0, result[1] or 0
 
 @session_wrapper
 def delete(beatmap_hash: str, user_id: int, session: Session = ...) -> None:
