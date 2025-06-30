ac9b6ae6ed00a3cb5bfc256812f3b84d5a9738b7
Lekuru <contact@lekuru.xyz>
2025-06-30 18:16:06 +0200

    Move `submit` function from deck to common

--- helpers/activity.py
+++ helpers/activity.py
@@ -1,8 +1,46 @@
 
+from app.common.database.repositories import activities, wrapper
 from app.common.database.objects import DBActivity
 from app.common.constants import UserActivity
+from app.common import officer
+from sqlalchemy.orm import Session
 
 import config
+import app
+
+def on_submit_fail(e: Exception) -> None:
+    officer.call(
+        f'Failed to submit highlight: "{e}"',
+        exc_info=e
+    )
+
+@wrapper.exception_wrapper(on_submit_fail)
+def submit(
+    user_id: int,
+    mode: int,
+    type: UserActivity,
+    data: dict,
+    session: Session,
+    is_announcement: bool = False,
+    is_hidden: bool = False
+) -> None:
+    app.session.events.submit(
+        'bancho_event',
+        user_id=user_id,
+        mode=mode,
+        type=type.value,
+        data=data,
+        is_announcement=is_announcement
+    )
+
+    if is_hidden:
+        return
+
+    activities.create(
+        user_id, mode,
+        type, data,
+        session=session
+    )
 
 def format_ranks_gained(activity: DBActivity, escape_brackets: bool = False) -> str:
     user_link = format_chat_link(
