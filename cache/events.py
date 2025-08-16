1e37716e6254629574314a0bca6e2fb1efcb2080
Lekuru <contact@lekuru.xyz>
2025-08-16 18:41:41 +0200

    Add `run` & `run_async` to `EventQueue`

--- cache/events.py
+++ cache/events.py
@@ -1,5 +1,4 @@
 
-from __future__ import annotations
 from typing import Tuple, Dict, Generator, Callable
 from threading import Thread
 from redis import Redis
@@ -8,9 +7,8 @@ import logging
 
 class EventQueue:
     def __init__(self, name: str, connection: Redis) -> None:
-        self.redis = connection
         self.name = name
-
+        self.redis = connection
         self.events: Dict[str, Callable] = {}
         self.logger = logging.getLogger(self.name)
         self.channel = self.redis.pubsub()
@@ -19,9 +17,7 @@ class EventQueue:
         """Register an event"""
         def wrapper(callback: Callable):
             self.events[event_name] = callback
-            self.logger.debug(
-                f'Registered new event: "{event_name}"'
-            )
+            self.logger.debug(f'Registered new event: "{event_name}"')
             return callback
         return wrapper
 
@@ -30,14 +26,17 @@ class EventQueue:
         self.redis.publish(self.name, str((event, args, kwargs)))
         self.logger.debug(f'Submitted event "{event}" to pubsub channel')
 
-    def poll(self) -> Tuple[Callable, Tuple, Dict] | None:
+    def poll(self, timeout: int = 0) -> Tuple[Callable, Tuple, Dict] | None:
         """Poll for events from the queue"""
         if self.name.encode() not in self.channel.channels:
             # Ensure we are subscribed to the channel
             self.channel.subscribe(self.name)
             self.logger.info(f'Subscribed to pubsub channel "{self.name}".')
 
-        message = self.channel.get_message(ignore_subscribe_messages=True)
+        message = self.channel.get_message(
+            ignore_subscribe_messages=True,
+            timeout=timeout
+        )
 
         if message is None:
             return
@@ -67,8 +66,26 @@ class EventQueue:
             except Exception as e:
                 self.logger.warning(f'Failed to evaluate task: {e}')
 
-    def listen_async(self) -> Thread:
-        """Listen for events in a separate thread"""
-        thread = Thread(target=self.listen, daemon=True)
+    def run(self, on_failure: Callable | None = None) -> None:
+        """Run the event loop"""
+        default_handler = lambda e: self.logger.error(
+            f'An error occurred while processing event: {e}',
+            exc_info=True
+        )
+
+        # Use default error handler, if on_failure is not provided
+        on_failure = on_failure or default_handler
+
+        for func, args, kwargs in self.listen():
+            try:
+                func(*args, **kwargs)
+            except SystemExit:
+                break
+            except Exception as e:
+                on_failure(e)
+
+    def run_async(self, on_failure: Callable | None = None) -> Thread:
+        """Listen & run events in a separate thread"""
+        thread = Thread(target=self.run, args=(on_failure,), daemon=True)
         thread.start()
         return thread
