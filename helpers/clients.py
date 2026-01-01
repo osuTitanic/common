3e50f332c0bf945e1c2214c83e49f50e00427d1c
Lekuru <contact@lekuru.xyz>
2026-01-01 22:03:18 +0100

    Add checks for release entry in `is_valid_mod` helper

--- helpers/clients.py
+++ helpers/clients.py
@@ -13,7 +13,11 @@ def is_valid_client_hash(version: int, hash: str, session: Session = ...) -> boo
 def is_valid_mod(identifier: str, hash: str, session: Session = ...) -> bool:
     if not (release := releases.fetch_modded(identifier, session)):
         return False
+    
+    if releases.fetch_modded_entry_by_checksum(release.name, hash, session):
+        return True
 
+    # TODO: Remove this in the future
     for entry in release.hashes:
         if entry['file'].endswith('.exe') and hash in entry['md5']:
             return True
