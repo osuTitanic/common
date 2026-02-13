61b9bdb3eb01b32310f33104d7f5cf24b48ea685
Lekuru <contact@lekuru.xyz>
2026-02-13 18:09:21 +0100

    Add file upload capabilities to webhook helper

--- webhooks.py
+++ webhooks.py
@@ -2,14 +2,12 @@
 # Modified webhook code from:
 # https://github.com/osuAkatsuki/bancho.py/blob/master/app/discord.py
 
-from __future__ import annotations
-
 from dataclasses import dataclass, field
-from requests import Response
 from datetime import datetime, timezone
-from typing import Any, List
+from typing import Any, List, Tuple
 
-import app
+import requests
+import json
 
 @dataclass
 class Footer:
@@ -84,8 +82,8 @@ class Webhook:
         username: str | None = None,
         avatar_url: str | None = None,
         is_tts: bool | None = None,
-        file: Any | None = None,
-        embeds: List[Embed] = []
+        file: Tuple[str, bytes] | None = None,
+        embeds: List[Embed] = [],
     ) -> None:
         self.url = url
         self.content = content
@@ -99,7 +97,7 @@ class Webhook:
     @property
     def json(self) -> Any:
         assert any([self.content, self.file, self.embeds]), (
-            "Webhook must contain at least one " "of (content, file, embeds)."
+            "Webhook must contain at least one of (content, file, embeds)."
         )
 
         if self.content and len(self.content) > 2000:
@@ -108,9 +106,8 @@ class Webhook:
 
         payload: dict[str, Any] = {"embeds": []}
 
-        for key in ("content", "username", "avatar_url", "tts", "file"):
+        for key in ("content", "username", "avatar_url", "tts"):
             val = getattr(self, key)
-
             if val is not None:
                 payload[key] = val
 
@@ -120,14 +117,12 @@ class Webhook:
             # Simple params
             for key in ("title", "type", "description", "url", "timestamp", "color"):
                 val = getattr(embed, key)
-
                 if val is not None:
                     embed_payload[key] = val
 
             # Class params, must turn into dict
             for key in ("footer", "image", "thumbnail", "video", "provider", "author"):
                 val = getattr(embed, key)
-
                 if val is not None:
                     embed_payload[key] = val.__dict__
 
@@ -141,34 +136,36 @@ class Webhook:
     def add_embed(self, embed: Embed) -> None:
         self.embeds.append(embed)
 
+    def set_file(self, filename: str, data: bytes) -> None:
+        self.file = (filename, data)
+
     def ensure_avatar_caching_disabled(self) -> None:
+        """Prevent Discord from caching avatars indefinetly, only cache for 1 day"""
         if type(self.avatar_url) != str:
             return
 
-        # Prevent Discord caching avatars indefinetly, only cache for 1 day
         now: datetime = datetime.now(timezone.utc)
         epoch: datetime = datetime(1970, 1, 1, tzinfo=timezone.utc)
         days_since_epoch = (now - epoch).days
         self.avatar_url = f"{self.avatar_url}?t={days_since_epoch}"
 
     def post(self) -> bool:
-        """Post the webhook in json format"""
-        try:
-            app.session.requests.post(
+        """Post the webhook"""
+        if self.file is None:
+            response = requests.post(
                 self.url,
                 json=self.json,
                 headers={
-                    "Content-Type": (
-                        "application/json"
-                        if self.file is None
-                        else "multipart/form-data"
-                    )
+                    "Content-Type": "application/json",
+                    "User-Agent": "osuTitanic/officer"
                 }
-            ).raise_for_status()
-            return True
-        except Exception as e:
-            app.session.logger.info(
-                f"Failed to post webhook: {e}",
-                exc_info=e
             )
-            return False
+            return response.ok
+
+        response = requests.post(
+            self.url,
+            headers={"User-Agent": "osuTitanic/officer"},
+            data={"payload_json": json.dumps(self.json)},
+            files={"file": self.file},
+        )
+        return response.ok
